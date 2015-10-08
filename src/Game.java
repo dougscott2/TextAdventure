0f9f440bee37d43b61dd6b310f928318bf70d008
DrScott <dougscott2@gmail.com>
2015-10-08 11:49:10 -0400

    initial import
    rename file from TextAdventure to Game
    
    added weapon and area select

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,43 @@
+import java.util.Scanner;
+
 /**
  * Created by DrScott on 10/8/15.
  */
 public class Game {
 
-    public static void run() {
+    public static void run() throws Exception {
         System.out.println("welcome to the jungle");
+        System.out.println("What is your name?");
+
+        Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+        System.out.println("Good luck, " + name );
+
+        System.out.println("[1] Pick up sword");
+        System.out.println("[2] Pick up mallet");
+        String weapon = scanner.nextLine();
+
+        if(weapon.equals("1")) {
+            System.out.println("that's a fine sword");
+        } else if(weapon.equals("2")){
+            System.out.println("That's a heavy mallet!");
+        } else {
+            throw new Exception("Invalid weapon, dummy");
+        }
+
+        System.out.println("[1] Enter the forest?");
+        System.out.println("[2] enter the tunnel?");
+        String area = scanner.nextLine();
+
+        if(area.equals("1")){
+            System.out.println("You enter the forest");
+        } else if(area.equals("2")){
+            System.out.println("you go into the tunnel");
+        } else {
+            throw new Exception("you can't go there, dummy");
+        }
+
+
+
     }
 }
