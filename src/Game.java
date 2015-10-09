4626a6a05f77f1617d6d91182bec6c513d1065f5
DrScott <dougscott2@gmail.com>
2015-10-09 11:19:53 -0400

    added while loop to keep game running until /exit is entered

--- src/Game.java
+++ src/Game.java
@@ -8,11 +8,12 @@ public class Game {
     public static void run() throws Exception {
         System.out.println("welcome to the jungle");
 
-        Player player = new Player();
-        player.chooseName();
-        player.chooseWeapon();
-        player.chooseArea();
-
+        while (true) {
+            Player player = new Player();
+            player.chooseName();
+            player.chooseWeapon();
+            player.chooseArea();
+        }
     }
     static String nextLine() {  //creating a static method to run to save the game
         Scanner scanner = new Scanner(System.in);
@@ -21,6 +22,8 @@ public class Game {
             if(s.equals("/help")){
                 System.out.println("These are the available commands");
                 System.out.println("/help    => List available commands");
+            } else if (s.equals("/exit")){
+                System.exit(0);
             }
             return nextLine();
         } else {
