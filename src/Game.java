2130873b5cce350d15b849c1712c8dc99b83b4f8
DrScott <dougscott2@gmail.com>
2015-10-09 09:56:12 -0400

    start work on game

--- src/Game.java
+++ src/Game.java
@@ -16,10 +16,10 @@ public class Game {
         System.out.println("[1] Pick up sword");
         System.out.println("[2] Pick up mallet");
         String weapon = scanner.nextLine();
-
-        if(weapon.equals("1")) {
+        int weaponNum = Integer.valueOf(weapon);
+        if(weaponNum == 1) {
             System.out.println("that's a fine sword");
-        } else if(weapon.equals("2")){
+        } else if(weaponNum == 2){
             System.out.println("That's a heavy mallet!");
         } else {
             throw new Exception("Invalid weapon, dummy");
@@ -28,10 +28,11 @@ public class Game {
         System.out.println("[1] Enter the forest?");
         System.out.println("[2] enter the tunnel?");
         String area = scanner.nextLine();
+        int areaNum = Integer.valueOf(area);
 
-        if(area.equals("1")){
+        if(areaNum == 1){
             System.out.println("You enter the forest");
-        } else if(area.equals("2")){
+        } else if(areaNum == 2){
             System.out.println("you go into the tunnel");
         } else {
             throw new Exception("you can't go there, dummy");
